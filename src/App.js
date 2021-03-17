923a0c8b471c9780e709c2b72b8cce7a8d8be859
nicolewallace09 <nicole.elisaw@gmail.com>
2021-03-16 23:25:30 -0700

    intial file set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget'; 
+
+function App() {
+  return (
+  <>
+  <Budget></Budget>
+  </> 
+  );
+}
+
+export default App;
